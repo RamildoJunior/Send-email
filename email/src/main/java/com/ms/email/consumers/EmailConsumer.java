aaadad309ca25c3ee41990ab75a8fd85bda2bd7e
RamildoJunior <rpsjuninho@gmail.com>
2025-02-19 20:30:58 -0300

    Concluindo a implementação do envio de email

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,13 +1,23 @@
 package com.ms.email.consumers;
 
 import com.ms.email.Dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 
 public class EmailConsumer {
 
+
+    @Autowired
+    EmailService emailService;
+
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void lisetenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
+       var emailModel = new EmailModel();
+       BeanUtils.copyProperties(emailRecordDto, emailModel);
+       emailService.sendEmail(emailModel);
     }
 }

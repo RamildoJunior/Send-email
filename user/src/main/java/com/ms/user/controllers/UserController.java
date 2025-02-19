b81a5e14f50528f5458fa0d2c877040a6690c9a2
RamildoJunior <rpsjuninho@gmail.com>
2025-02-19 09:33:53 -0300

    Criando o Producer

--- user/src/main/java/com/ms/user/controllers/UserController.java
+++ user/src/main/java/com/ms/user/controllers/UserController.java
@@ -1,13 +1,12 @@
 package com.ms.user.controllers;
 
-import com.ms.user.dto.UserRecordDto;
+import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
 import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- user/src/main/java/com/ms/user/dtos/UserRecordDto.java
+++ user/src/main/java/com/ms/user/dtos/UserRecordDto.java
@@ -1,4 +1,4 @@
-package com.ms.user.dto;
+package com.ms.user.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
